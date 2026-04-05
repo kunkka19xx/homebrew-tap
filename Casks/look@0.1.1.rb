6c09b284e8d7051ec15263269fc4cefcf26243ce
kunkka19xx <nobita079x@gmail.com>
2026-04-05 20:08:46 +0900

    new ver

--- Casks/look@0.1.1.rb
+++ Casks/look@0.1.1.rb
@@ -1,18 +1,22 @@
-cask "look" do
-  # Update these values for each release.
+cask "look@0.1.1" do
   owner = "kunkka19xx"
   repo = "look"
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
 
-  version "0.1.2"
-  sha256 "e305a0e59005db9ef3e2fa3f8a19fd7e1efd5a8657a5962df7a1546dcec452cb"
+  version "0.1.1"
+  sha256 "de976e6441c650ac49487dbe79b74490d55b5411bf0561a6f423b9c85aab2439"
 
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
-  name repo
+  name "look"
   desc "Keyboard-first local launcher for macOS"
   homepage "https://github.com/#{owner}/#{repo}"
 
+  livecheck do
+    skip "Versioned cask"
+  end
+
+  conflicts_with cask: ["look", "look@0.1"]
   app "#{app_name}.app"
 end
