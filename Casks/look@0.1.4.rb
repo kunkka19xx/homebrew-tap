96bb70c3402a89c2c5b1582823b4e69612b183b6
kunkka19xx <nobita079x@gmail.com>
2026-04-05 20:40:44 +0900

    fix version issue again

--- Casks/look@0.1.4.rb
+++ Casks/look@0.1.4.rb
@@ -1,15 +1,22 @@
-cask "look" do
+cask "look@0.1.4" do
   owner = "kunkka19xx"
   repo = "look"
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.1.5"
-  sha256 "f8141eac494c837c52439050d7fdc79d1570d2de76e84753b31b57fee6c8ca93"
+
+  version "0.1.4"
+  sha256 "f43a3e01e4f44e94f9efbfa2bfa964143dfd76d4ecd4d1f39d0677305c1936e0"
+
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
-  name repo
+  name "look"
   desc "Keyboard-first local launcher for macOS"
   homepage "https://github.com/#{owner}/#{repo}"
+
+  livecheck do
+    skip "Versioned cask"
+  end
+
+  conflicts_with cask: ["look", "look@0.1", "look@0.1.1", "look@0.1.2", "look@0.1.3"]
   app "#{app_name}.app"
-  binary "#{app_name}.app/Contents/MacOS/Look", target: "lookapp"
 end
