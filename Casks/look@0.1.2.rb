b2d65acb9e98e8e801dfe57f4ea3924d9c07f4ab
kunkka19xx <nobita079x@gmail.com>
2026-04-05 20:21:54 +0900

    fix app name issue

--- Casks/look@0.1.2.rb
+++ Casks/look@0.1.2.rb
@@ -1,15 +1,22 @@
-cask "look" do
+cask "look@0.1.2" do
   owner = "kunkka19xx"
   repo = "look"
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.1.3"
-  sha256 "b248c0debf414928271c976ba1870bc7a10ccf563ab02df1b62a630949b2dc27"
+
+  version "0.1.2"
+  sha256 "e305a0e59005db9ef3e2fa3f8a19fd7e1efd5a8657a5962df7a1546dcec452cb"
+
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
-  name repo
+  name "look"
   desc "Keyboard-first local launcher for macOS"
   homepage "https://github.com/#{owner}/#{repo}"
+
+  livecheck do
+    skip "Versioned cask"
+  end
+
+  conflicts_with cask: ["look", "look@0.1", "look@0.1.1"]
   app "#{app_name}.app"
-  binary "#{app_name}.app/Contents/MacOS/Look", target: "lookapp"
 end
