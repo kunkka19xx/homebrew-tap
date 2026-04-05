5f1b8e84a6d497e9f7087516c4f53c0650286763
kunkka19xx <nobita079x@gmail.com>
2026-04-05 20:14:33 +0900

    fix app name

--- Casks/look.rb
+++ Casks/look.rb
@@ -1,18 +1,15 @@
 cask "look" do
-  # Update these values for each release.
   owner = "kunkka19xx"
   repo = "look"
-  app_name = "lookapp"
+  app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-
   version "0.1.2"
   sha256 "e305a0e59005db9ef3e2fa3f8a19fd7e1efd5a8657a5962df7a1546dcec452cb"
-
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
   name repo
   desc "Keyboard-first local launcher for macOS"
   homepage "https://github.com/#{owner}/#{repo}"
-
   app "#{app_name}.app"
+  binary "#{app_name}.app/Contents/MacOS/Look", target: "lookapp"
 end
