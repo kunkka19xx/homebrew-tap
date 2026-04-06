1a67c3087f10c2f28e50036794b7f11847819dde
kunkka19xx <nobita079x@gmail.com>
2026-04-06 16:09:58 +0900

    new features

--- Casks/look@0.1.7.rb
+++ Casks/look@0.1.7.rb
@@ -1,15 +1,22 @@
-cask "look" do
+cask "look@0.1.7" do
   owner = "kunkka19xx"
   repo = "look"
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.1.8"
-  sha256 "6c833979ed9df0c69a30271ef9ed6800ea8494467e56a383c362d7bdd15806be"
+
+  version "0.1.7"
+  sha256 "97b6b45d266751140737b55b0c03c9d887605fa89c735c45c22ec682727bc3f1"
+
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
-  name repo
+  name "look"
   desc "Keyboard-first local launcher for macOS"
   homepage "https://github.com/#{owner}/#{repo}"
+
+  livecheck do
+    skip "Versioned cask"
+  end
+
+  conflicts_with cask: ["look", "look@0.1.1", "look@0.1.2", "look@0.1.3", "look@0.1.4", "look@0.1.5", "look@0.1.6", "look@0.1"]
   app "#{app_name}.app"
-  binary "#{app_name}.app/Contents/MacOS/Look", target: "lookapp"
 end
