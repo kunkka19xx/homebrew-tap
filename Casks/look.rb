3388859e2080849a9965f7f9e7bccdbf985d9876
kunkka19xx <nobita079x@gmail.com>
2026-04-13 17:56:56 +0900

    notarizedddd

--- Casks/look.rb
+++ Casks/look.rb
@@ -4,8 +4,8 @@ cask "look" do
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "2.0.5"
-  sha256 "475f9d9e71ebc8541a13aeea90eedb0c8d3f4a6f0968b75672394480abf21ee6"
+  version "0.3.0"
+  sha256 "402a0e18ebb643b0b62d2f7e664a13643939d4394dbb2b6703bf4321b59f23e2"
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
   name repo
   desc "Keyboard-first local launcher for macOS"
