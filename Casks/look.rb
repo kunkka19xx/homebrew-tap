c62ca0243d3c57764927e7f2719c2c820c05501b
kunkka19xx <nobita079x@gmail.com>
2026-05-06 12:00:10 +0900

    fix pomo issue

--- Casks/look.rb
+++ Casks/look.rb
@@ -4,8 +4,8 @@ cask "look" do
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.4.3"
-  sha256 "374088682319454207feda750531ca845221517cdca36acafb8df852d87b1b66"
+  version "0.4.4"
+  sha256 "a4a5b0ac1da5ab71e2ff6712815f3818a216cd5b6a9dacdfe6b0f0f045e49101"
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
   name repo
   desc "Keyboard-first local launcher for macOS"
