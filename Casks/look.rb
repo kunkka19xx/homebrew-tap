b0f858e30dc5195e42089598fc8e9270a0facf2a
kunkka19xx <nobita079x@gmail.com>
2026-05-30 11:58:26 +0900

    smart app switching

--- Casks/look.rb
+++ Casks/look.rb
@@ -4,8 +4,8 @@ cask "look" do
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.5.7"
-  sha256 "505cd2569b36d66ab1e7fc01f333b8a0f2ad7ce7e76c837fcb68bcc60f3bd75e"
+  version "0.5.8"
+  sha256 "37207c5d428bc7ff8c4b209445928242321b7f4e33c348169835153d71e7eb1d"
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
   name repo
   desc "Keyboard-first local launcher for macOS"
---
+++ Casks/look@0.5.7.rb
@@ -0,0 +1,22 @@
+cask "look@0.5.7" do
+  owner = "kunkka19xx"
+  repo = "look"
+  app_name = "Look"
+  release_tag_prefix = "v"
+  release_asset_suffix = "macOS.zip"
+
+  version "0.5.7"
+  sha256 "505cd2569b36d66ab1e7fc01f333b8a0f2ad7ce7e76c837fcb68bcc60f3bd75e"
+
+  url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
+  name "look"
+  desc "Keyboard-first local launcher for macOS"
+  homepage "https://github.com/#{owner}/#{repo}"
+
+  livecheck do
+    skip "Versioned cask"
+  end
+
+  conflicts_with cask: ["look", "look@0.1.1", "look@0.1.2", "look@0.1.3", "look@0.1.4", "look@0.1.5", "look@0.1.6", "look@0.1.7", "look@0.1.8", "look@0.1.9", "look@0.1", "look@0.2.0", "look@0.2.1", "look@0.2.2", "look@0.2.3", "look@0.3.0", "look@0.3.1", "look@0.3.2", "look@0.3.3", "look@0.3.4", "look@0.3.5", "look@0.3.6", "look@0.3.7", "look@0.3.8", "look@0.3.9", "look@0.4.1", "look@0.4.2", "look@0.4.3", "look@0.4.4", "look@0.5.3", "look@2.0.4", "look@2.0.5"]
+  app "#{app_name}.app"
+end
