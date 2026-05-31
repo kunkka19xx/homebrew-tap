6d26723d2f3eda12cbd554fd870213a585544fb9
kunkka19xx <nobita079x@gmail.com>
2026-05-31 15:46:01 +0900

    change ui of smart app switching

--- Casks/look.rb
+++ Casks/look.rb
@@ -4,8 +4,8 @@ cask "look" do
   app_name = "Look"
   release_tag_prefix = "v"
   release_asset_suffix = "macOS.zip"
-  version "0.5.8"
-  sha256 "37207c5d428bc7ff8c4b209445928242321b7f4e33c348169835153d71e7eb1d"
+  version "0.6.0"
+  sha256 "a1f9e67e92d6f5dba6528a77d3bf2a65e3b9ca1f6fde381d1de80e4f1588c14c"
   url "https://github.com/#{owner}/#{repo}/releases/download/#{release_tag_prefix}#{version}/#{app_name}-#{version}-#{release_asset_suffix}"
   name repo
   desc "Keyboard-first local launcher for macOS"
